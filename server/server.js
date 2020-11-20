eb7ac4f0788b08341c7b232789f17dd2cc14714b
Henry Fradley <fradleyhenry@gmail.com>
2020-11-20 15:15:11 -0800

    changed port number

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 3005;
 const bodyParser = require('body-parser');
 const database = require('./database/db.js');
 
